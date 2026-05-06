7f04173705e79c8bc9489d1fb9185c1e9f10ebe2
dallenpyrah <dallenpyrah@gmail.com>
2026-05-05 23:02:33 -0600

    feat: add process runtime service
    Closes #107

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,6 +3,7 @@ import { Api, Client, Handlers } from "@effect-desktop/bridge"
 export { Api, Client, Handlers } from "@effect-desktop/bridge"
 export * from "./runtime/resources.js"
 export * from "./runtime/filesystem.js"
+export * from "./runtime/process.js"
 export * from "./runtime/window-state.js"
 
 export const Desktop = Object.freeze({
